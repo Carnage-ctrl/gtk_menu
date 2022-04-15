52edba067117abc0b2249f4af2086913449e61c3
Carnage <85742443+Carnage-ctrl@users.noreply.github.com>
2022-04-15 12:42:24 -0300

    Add files via upload

---
+++ page.h
@@ -0,0 +1,8 @@
+#ifndef PAGE_H // PAGE_H
+#define PAGE_H // PAGE_H
+
+GtkWidget* Window, *Box, *MenuBar, *SettingsMenu, *SettingsMenuItem, *SettingsMenuCloseItem, *Dialog;
+
+extern void encerrar();
+
+#endif // PAGE_H
